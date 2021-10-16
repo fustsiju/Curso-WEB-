57c81a771fe71a7129f720382fd7af23a058eefd
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2021-10-16 19:18:18 -0300

    Add files via upload

---
+++ Iniciando_JS/teste.js
@@ -0,0 +1,9 @@
+let username = "fulvin";
+let password = "fulvin123";
+
+
+if (username == "fulvio" && password == "parabola32") {
+    console.log("Usuário autenticado");
+} else {
+    console.log("Login e/ou senha incorrretos");
+}
