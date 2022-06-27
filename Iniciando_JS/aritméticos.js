cbe3c2f2844c5ed2691e5762116ce3b4caabd141
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2022-06-27 09:54:51 -0300

    Add files via upload

---
+++ Iniciando_JS/aritméticos.js
@@ -0,0 +1,9 @@
+const [a,b,c,d] = [10,2,5,20]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
