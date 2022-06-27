3b0a2bd6c7df535ed6a1b6b8dfd26ace5bc488a3
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2022-06-27 09:53:59 -0300

    Add files via upload

---
+++ Iniciando_JS/Destructuring1.js
@@ -0,0 +1,20 @@
+const pessoa = {
+    nome: 'Ana',
+    idade: '19',
+    endereco: {
+        logradouro: 'Rua Jajaja',
+        numero: '928'
+    }
+}
+
+const {nome, idade} = pessoa //Tirando nome e idade da constante "pessoa", método para retirar vários atributos de um objeto sem imprimir ele todo.
+console.log(nome, idade)
+
+const {nome: n, idade: i } = pessoa
+console.log(n, i)
+
+const {sobrenome, bemHumorada = true } = pessoa
+console.log(sobrenome, bemHumorada)
+
+const{endereco: {logradouro, numero, cep } } = pessoa
+console.log(logradouro, numero, cep)
