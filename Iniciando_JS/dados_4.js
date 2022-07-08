4b09a199a7419e3de1afcb010937edc1e85a705e
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2022-07-07 22:53:06 -0300

    Update dados_4.js

--- Iniciando_JS/dados_4.js
+++ Iniciando_JS/dados_4.js
@@ -1,10 +1,9 @@
 const a = 3 // let a = 3     
 
-// ...                         Constante(const) não pode ser
+//                          Constante(const) não pode ser
                             // modificada, priorizar em programas
                             // que não precise modificar alguns valores.
 // a = a + 10
 
-//...
 
 console.log(a)
